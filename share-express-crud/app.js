a43b19a9f91e8e47ffc94fda2dae86430c4bee95
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-09-11 15:11:16 +0530

    Add files via upload

--- share-express-crud/app.js
+++ share-express-crud/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
+const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(
+    // "mongodb://root:root@172.30.0.2:27017/?authSource=admin")
+    // "mongodb://root:root@mongo:27017/?authSource=admin")
+    MONGO_URL)
+    .then(() => {
+        console.log("Successfully connected to MongoDB");
+    })
+    .catch((e) => {
+        console.log("Error :",e);
+    });
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World using Docker and Express</h1>");
