0eb3a66a2c07018b2085dc456a625b6c98100abb
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-09-11 14:03:44 +0530

    Add files via upload

--- share-express-crud/app.js
+++ share-express-crud/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World using Docker</h1>");
+    res.send("<h1>Hello World using Docker and Express</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
